5cacc1f1ba8e32299ea4673c7299e69e22184aa1
davzer27 <david.xox@gmail.com>
2018-03-06 17:53:29 +0000

    sprint 1

--- backend/src/api/courses/index.js
+++ backend/src/api/courses/index.js
@@ -7,7 +7,7 @@ const controller = require('./courses.controller');
 
 /**
 * @apiVersion 1.0.0-SNAPSHOT
- * @api {get} /api/courses/ getUserRole
+ * @api {get} /courses/ getCourses
  * @apiDescription récupère la liste de tous les cours
  * @apiName getCourses
  * @apiGroup Cours
@@ -36,6 +36,42 @@ const controller = require('./courses.controller');
  */
 router.get('/', controller.findAll);
 
+/**
+* @apiVersion 1.0.0-SNAPSHOT
+ * @api {post} /courses/ postCourses
+ * @apiDescription crée un cours
+ * @apiName postCourses
+ * @apiGroup Cours
+ * @apiExample Example usage:
+ *
+ *     body:
+ * {
+ * "title" : "Android",
+ * "promo" : [
+ * {
+ * "nomPromo" : "G3",
+ * "membres" : [{
+ *  "username" : "davzer"
+ * },
+ * {
+ * "username" : "zizid"
+ * }
+ * ],
+ * "responsable" : "TBH",
+ *}
+ *]
+ *}
+ *
+ * @apiSuccess {Boolean} success API response
+ * @apiSuccessExample {json} Success-Response:
+ *{
+ *  success : true
+ *}
+
+ *
+ * @apiError (500) Mongoose Error
+ * @apiError (404) Missing Fields
+ */
 router.post('/', controller.create);
 
 
