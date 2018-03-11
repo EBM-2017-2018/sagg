33c992f82de476e21205c3c82e4310b014555ea2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-11 12:43:10 +0000

    Deploy EBM-2017-2018/sagg to github.com/EBM-2017-2018/sagg.git:gh-pages

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "title": "ApiDoc pour le projet SAGG",
+  "url": "/api",
+  "sampleUrl": "http://localhost:4000/api",
+  "name": "sagg-backend",
+  "version": "0.1.0",
+  "description": "",
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-03-11T12:42:37.073Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
