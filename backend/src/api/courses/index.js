5de46892b91306cf63574d554a45ddeca0688216
davzer27 <david.xox@gmail.com>
2018-03-12 11:26:56 +0000

    new changes to integrate JWT auth

--- backend/src/api/courses/index.js
+++ backend/src/api/courses/index.js
@@ -9,13 +9,15 @@ const controller = require('./courses.controller');
 * @apiVersion 1.0.0-SNAPSHOT
  * @api {get} /promos/:pid/courses getCourses
  * @apiDescription récupère la liste de tous les cours associés à une promo
+ * Attention le rôle du token doit être différent d'un étudiant pour y acceder
  * @apiName getCourses
  * @apiParam {String} pid Id de la promo
+ * @apiHeader {String} Authorization JWT token
  * @apiGroup Cours
  * @apiSuccess {courses[]} courses liste des cours
  * @apiSuccessExample {json} Success-Response:
  *{
- *  [
+ *  courses: [
  *      {
  *          "promoId" : "5b9ea41a4e2d37b1400c7b7a",
  *          "title" : "Cours 1",
@@ -36,11 +38,16 @@ const controller = require('./courses.controller');
  *                  ],
  *          "_id": "5a9ea41a4e7d37b1400c7b7e",
  *      }
- *  ]
+ *  ],
+ *    "new_access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6
+ * InJvb3QiLCJyb2xlIjoiZXR1ZGlhbnQiLCJub20iOiJyb290IiwicHJlbm9tIjoicm9vdCIsI
+ * mVtYWlsIjoicm9vdEBldHVkaWFudC5mciIsImlhdCI6MTUyMDg1MjkxN30.fbHx4vToa-K9pAiVGn1q-6n3N
+ * -vH7tk_DMQssqk-5w0"
  *}
 
  *
  * @apiError (500) Mongoose Error
+ * @apiError (404) Missing/WrongToken Fields
  */
 router.get('/:pid/courses', controller.findAll);
 
@@ -49,6 +56,7 @@ router.get('/:pid/courses', controller.findAll);
  * @api {post} /promos/:pid/courses postCourses
  * @apiDescription Crée un cours associé à une promo, date au format ISO8601
  * @apiName postCourses
+ * @apiHeader {String} Authorization JWT token
  * @apiParam {String} pid Id de la promo
  * @apiGroup Cours
 
@@ -80,12 +88,15 @@ router.get('/:pid/courses', controller.findAll);
  * @apiSuccess {Boolean} success API response
  * @apiSuccessExample {json} Success-Response:
  *{
- *  success : true
+ * success : true
+ * new_access_token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJ
+ * vb3QiLCJyb2xlIjoiZXR1ZGlhbnQiLCJub20iOiJyb290IiwicHJlbm9tIjoicm9vdCIsImVtYWlsIjo
+ * icm9vdEBldHVkaWFudC5mciIsImlhdCI6MTUyMDg1MjkxN30.fbHx4vToa-K9pAiVGn1q-6n3N-vH7tk_DMQssqk-5w0
  *}
 
  *
  * @apiError (500) Mongoose Error
- * @apiError (404) Missing Fields
+ * @apiError (404) Missing/WrongToken Fields
  */
 router.post('/:pid/courses', controller.create);
 
@@ -94,6 +105,7 @@ router.post('/:pid/courses', controller.create);
  * @api {put} /promos/courses/:cid putCourses
  * @apiDescription Modifier la présence des élèves à un cours, id promo non nécess
  * @apiName putCourses
+ * @apiHeader {String} Authorization JWT token
  * @apiParam {String} cid Id du cours
  * @apiGroup Cours
 
@@ -126,11 +138,14 @@ router.post('/:pid/courses', controller.create);
  * @apiSuccessExample {json} Success-Response:
  *{
  *  success : true
+ * new_access_token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJ
+ * vb3QiLCJyb2xlIjoiZXR1ZGlhbnQiLCJub20iOiJyb290IiwicHJlbm9tIjoicm9vdCIsImVtYWlsIjo
+ * icm9vdEBldHVkaWFudC5mciIsImlhdCI6MTUyMDg1MjkxN30.fbHx4vToa-K9pAiVGn1q-6n3N-vH7tk_DMQssqk-5w0
  *}
 
  *
  * @apiError (500) Mongoose Error
- * @apiError (404) Missing Fields
+ * @apiError (404) Missing/WrongToken Fields
  */
 
 router.put('/courses/:cid', controller.update);
