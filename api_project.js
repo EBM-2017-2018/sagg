2a543f7ba23f72c0e9e8020ad7e9aacd9625a978
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-27 23:27:45 +0000

    Deploy EBM-2017-2018/sagg to github.com/EBM-2017-2018/sagg.git:gh-pages

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-03-27T23:07:05.506Z",
+    "time": "2018-03-27T23:27:10.516Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
