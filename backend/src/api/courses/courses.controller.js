b7898bcecd3e21393ae4c1d76b90396a47b7f0b0
davzer27 <david.xox@gmail.com>
2018-03-27 12:03:22 +0100

    Add the delete possibilities course

--- backend/src/api/courses/courses.controller.js
+++ backend/src/api/courses/courses.controller.js
@@ -134,3 +134,32 @@ module.exports.update = (req, res) => {
   );
   return null;
 };
+module.exports.delete = (req, res) => {
+  if (!req.params.cid) {
+    return res.status(404).json({
+      code: 'MISSING_PROMO_OR_COURSE',
+      success: false,
+      message: 'Le cours n\'est pas defini',
+    });
+  }
+  if (req.user && req.user.role === 'etudiant') {
+    return res.status(403).json({
+      code: 'UNAUTHORIZED',
+      success: false,
+      message: 'Cet action est uniquement réservée à un professeur/intervant.',
+    });
+  }
+  Course.remove(
+    { _id: req.params.cid },
+    (err) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+      return res.status(200).json({
+        success: true,
+        message: 'Le cours a correctement été supprumé.',
+      });
+    },
+  );
+  return null;
+};
