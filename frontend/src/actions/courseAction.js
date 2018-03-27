b2e40ebb47cad8d28afd7fc191f53bc495108d79
ebooth <pauldejean01@gmail.com>
2018-03-28 01:02:16 +0200

    oups

--- frontend/src/actions/courseAction.js
+++ frontend/src/actions/courseAction.js
@@ -21,7 +21,7 @@ export function saveCourse(promoId, course) {
 
         var myInit = {
             method: 'POST',
-            headers: Object.assign({}, getAuthHeaders(), {'Accept': 'application/json, text/plain, */*' , 'Content-Type': 'application/json'})
+            headers: Object.assign({}, getAuthHeaders(), {'Accept': 'application/json, text/plain, */*' , 'Content-Type': 'application/json'}),
             mode: 'cors',
             cache: 'default',
             body: JSON.stringify(body)
